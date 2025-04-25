8b61b76d76aac7c7a40f1ff9ec6aa2e23af8ecc9
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2025-04-26 02:02:16 +0530

    New modules added

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
